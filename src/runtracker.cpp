1774cc87070bbcccc259621766b181b2d3c97117
snuzhny <nuzhny@mail.ru>
2025-04-22 13:11:02 +0300

    Draw ground truth on all frames

--- src/runtracker.cpp
+++ src/runtracker.cpp
@@ -2,6 +2,7 @@
 #include <fstream>
 #include <sstream>
 #include <algorithm>
+#include <filesystem>
 
 #include <opencv2/opencv.hpp>
 
@@ -46,48 +47,62 @@ int main(int argc, char* argv[])
 
   	// Read groundtruth for the 1st frame
   	std::ifstream groundtruthFile;
-	std::string groundtruth = parser.get<std::string>("region");
-  	groundtruthFile.open(groundtruth);
-  	std::string firstLine;
-  	std::getline(groundtruthFile, firstLine);
-	groundtruthFile.close();
+  	groundtruthFile.open(parser.get<std::string>("region"));
   	
-	std::istringstream ss(firstLine);
+	auto GetRectFromFile = [&groundtruthFile]()
+		{
+			std::string regionLine;
+			std::getline(groundtruthFile, regionLine);
 
-  	// Read groundtruth like a dumb
-  	float x1, y1, x2, y2, x3, y3, x4, y4;
-  	char ch;
-	ss >> x1;
-	ss >> ch;
-	ss >> y1;
-	ss >> ch;
-	ss >> x2;
-	ss >> ch;
-	ss >> y2;
-	ss >> ch;
-	ss >> x3;
-	ss >> ch;
-	ss >> y3;
-	ss >> ch;
-	ss >> x4;
-	ss >> ch;
-	ss >> y4; 
+			std::istringstream ss(regionLine);
 
-	// Using min and max of X and Y for groundtruth rectangle
-	float xMin =  std::min(x1, std::min(x2, std::min(x3, x4)));
-	float yMin =  std::min(y1, std::min(y2, std::min(y3, y4)));
-	float width = std::max(x1, std::max(x2, std::max(x3, x4))) - xMin;
-	float height = std::max(y1, std::max(y2, std::max(y3, y4))) - yMin;
+			// Read groundtruth like a dumb
+			float x1, y1, x2, y2, x3, y3, x4, y4;
+			char ch;
+			ss >> x1;
+			ss >> ch;
+			ss >> y1;
+			ss >> ch;
+			ss >> x2;
+			ss >> ch;
+			ss >> y2;
+			ss >> ch;
+			ss >> x3;
+			ss >> ch;
+			ss >> y3;
+			ss >> ch;
+			ss >> x4;
+			ss >> ch;
+			ss >> y4;
+
+			// Using min and max of X and Y for groundtruth rectangle
+			float xMin = std::min(x1, std::min(x2, std::min(x3, x4)));
+			float yMin = std::min(y1, std::min(y2, std::min(y3, y4)));
+			float width = std::max(x1, std::max(x2, std::max(x3, x4))) - xMin;
+			float height = std::max(y1, std::max(y2, std::max(y3, y4))) - yMin;
+
+			return cv::Rect(cvRound(xMin), cvRound(yMin), cvRound(width), cvRound(height));
+		};
 	
 	// Read Images
 	std::ifstream listFramesFile;
-	std::string listFrames = parser.get<std::string>("images");
-	listFramesFile.open(listFrames);
+	std::filesystem::path listFramesPath = parser.get<std::string>("images");
+	listFramesFile.open(listFramesPath.string());
+	if (!listFramesFile.is_open())
+	{
+		std::cout << "File " << listFramesPath.string() << " not opened!" << std::endl;
+		return 1;
+	}
 
 	// Write Results
 	std::ofstream resultsFile;
 	std::string resultsPath = parser.get<std::string>("output");
 	resultsFile.open(resultsPath);
+	if (!resultsFile.is_open())
+	{
+		std::cout << "File " << resultsPath << " not opened!" << std::endl;
+		return 2;
+	}
 
 	// Frame counter
 	int nFrames = 0;
@@ -97,22 +112,35 @@ int main(int argc, char* argv[])
 	while (getline(listFramesFile, frameName))
 	{
 		// Read each frame from the list
-		frame = cv::imread(frameName, cv::IMREAD_COLOR);
+		std::string pathToFrame = (listFramesPath.remove_filename() / frameName).string();
+		frame = cv::imread(pathToFrame, cv::IMREAD_COLOR);
+		if (frame.empty())
+		{
+			std::cout << "Frame " << pathToFrame << " is empty" << std::endl;
+		}
+
+		cv::Rect gtRect = GetRectFromFile();
+
+		std::cout << "Ground truth " << gtRect << " on frame " << frame.size() << std::endl;
 
 		// First frame, give the groundtruth to the tracker
 		if (nFrames == 0)
 		{
-			tracker.init(cv::Rect(xMin, yMin, width, height), frame );
-			rectangle( frame, cv::Point( xMin, yMin ), cv::Point( xMin+width, yMin+height), cv::Scalar( 0, 255, 255 ), 1, 8 );
-			resultsFile << xMin << "," << yMin << "," << width << "," << height << std::endl;
+			tracker.init(gtRect, frame);
+			resultsFile << gtRect.x << "," << gtRect.y << "," << gtRect.width << "," << gtRect.height << std::endl;
 		}
 		// Update
 		else
 		{
 			cv::Rect result = tracker.update(frame);
-			rectangle( frame, cv::Point( result.x, result.y ), cv::Point( result.x+result.width, result.y+result.height), cv::Scalar( 0, 255, 255 ), 1, 8 );
+
+			std::cout << "Detected rect " << result << ", IoU = " << ((gtRect & result).area() / (float)(gtRect | result).area()) << std::endl;
+
+			cv::rectangle(frame, result.tl(), result.br(), cv::Scalar(255, 0, 255), 1, 8);
 			resultsFile << result.x << "," << result.y << "," << result.width << "," << result.height << std::endl;
 		}
+
+		cv::rectangle(frame, gtRect.tl(), gtRect.br(), cv::Scalar(0, 255, 0), 1, 8);
 
 		nFrames++;
 
